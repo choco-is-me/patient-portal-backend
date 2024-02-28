66cb58e57c8fca913633875ab79951abff09185b
Trần Thanh Bình <binhtranthanhbinh120@gmail.com>
2024-02-28 20:56:57 +0700

    update

--- app.js
+++ app.js
@@ -136,6 +136,11 @@ Role.find({}).then(async (roles) => {
     }
 });
 
+process.on("unhandledRejection", (reason, p) => {
+    console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
 // Helper function to check if two arrays are equal
 function arraysEqual(a, b) {
     return a.sort().toString() === b.sort().toString();
