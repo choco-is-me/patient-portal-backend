3e5679a9237365caf918333f66d6120440661b94
Trần Thanh Bình <binhtranthanhbinh120@gmail.com>
2024-02-28 17:05:20 +0700

    update mongoose

--- connectMongo.js
+++ connectMongo.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_CONNECT_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of the default 30s
         });
         console.log("Connect to MongoDB successfully");
