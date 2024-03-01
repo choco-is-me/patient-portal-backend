cc6e0dc89007eef71d73c25a46b3a3e437cc31f0
Trần Thanh Bình <binhtranthanhbinh120@gmail.com>
2024-03-01 15:11:21 +0700

    test

--- devServer.js
+++ devServer.js
@@ -1,11 +1,15 @@
 const app = require("./app");
 const routes = require("./routes/router");
+const adminRouter = require("./routes/admin");
+const doctorRouter = require("./routes/doctor");
+const nurseRouter = require("./routes/nurse");
+const patientRouter = require("./routes/patient");
 
 app.use("/", routes);
-app.use("/api/admin", routes);
-app.use("/api/doctor", routes);
-app.use("/api/nurse", routes);
-app.use("/api/patient", routes);
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/nurse", nurseRouter);
+app.use("/api/patient", patientRouter);
 
 // start the server listening for requests
 const port = process.env.PORT;
